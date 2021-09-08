41ce58686a921f3328ab83603024f54198363022
Rodrigo <rodrigo.siqueira@rdsti.com.br>
2021-09-07 22:53:55 -0300

    Project controle-financeiro-bll e controle-financeiro-dal created

---
+++ controle-financeiro-bll/Models/Categoria.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace controle_financeiro_bll.Models
+{
+    class Categoria
+    {
+        public int CategoriaId { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Icone { get; set; }
+
+        public int TipoId { get; set; }
+
+        public Tipo Tipo { get; set; }
+
+        public virtual ICollection<Despesa> Despesas { get; set; }
+
+        public virtual ICollection<Ganho> Ganhos { get; set; }
+    }
+}
---
+++ controle-financeiro-bll/Models/tipo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace controle_financeiro_bll.Models
+{
+    class tipo
+    {
+        public int TipoId { get; set; }
+
+        public string Nome { get; set; }
+
+        public virtual ICollection<Categoria> GetCategorias { get; set; }
+
+    }
+}
