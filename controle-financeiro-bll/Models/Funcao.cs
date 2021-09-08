8f9a1bb967c7b86a68002ae688c1a929e404b194
Rodrigo <rodrigo.siqueira@rdsti.com.br>
2021-09-08 01:00:37 -0300

    Map Cartao e Mes created

--- controle-financeiro-bll/Models/Funcao.cs
+++ controle-financeiro-bll/Models/Funcao.cs
@@ -1,10 +1,12 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace controle_financeiro_bll.Models
 {
-    class Funcao
+    public class Funcao : IdentityRole<string>
     {
+        public string Descricao { get; set; }
     }
 }
