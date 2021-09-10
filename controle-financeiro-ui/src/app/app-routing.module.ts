3aa857a8a3d7d4c6fd768ab6f572e9e5c36037ca
Rodrigo <rodrigo.siqueira@rdsti.com.br>
2021-09-10 02:04:39 -0300

    Criação do formulário Categoria Reactive Forms

--- controle-financeiro-ui/src/app/app-routing.module.ts
+++ controle-financeiro-ui/src/app/app-routing.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ListagemCategoriasComponent } from './components/Categoria/listagem-categorias/listagem-categorias.component';
+import { NovaCategoriaComponent } from './components/Categoria/nova-categoria/nova-categoria.component';
 
 
 const routes: Routes = [
   {
     path : 'categorias/listagemCategorias', component: ListagemCategoriasComponent
+  },
+
+  {
+    path : 'categorias/novaCategorias', component: NovaCategoriaComponent
   }
+
 ];
 
 @NgModule({
