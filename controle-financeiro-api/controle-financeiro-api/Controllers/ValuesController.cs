f958f152e67375604d32d6bf4874741e6334dc53
Rodrigo <rodrigo.siqueira@rdsti.com.br>
2021-09-10 00:01:44 -0300

    Criando o primeiro serviço no Angular

---
+++ controle-financeiro-api/controle-financeiro-api/Controllers/ValuesController.cs
@@ -0,0 +1,33 @@
+﻿using controle_financeiro_bll.Models;
+using controle_financeiro_dal;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace controle_financeiro_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TiposController : ControllerBase
+    {
+        private readonly Contexto _context;
+
+        public TiposController(Contexto context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Tipo>>> GetTipos()
+        {
+            return await _context.Tipos.ToListAsync();
+        }
+
+    }
+
+}
