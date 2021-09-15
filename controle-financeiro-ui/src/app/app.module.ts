21920bba39649d98f62ee6398812c48827cb3801
Rodrigo <rodrigo.siqueira@rdsti.com.br>
2021-09-15 02:10:55 -0300

    Exclusão a categoria

--- controle-financeiro-ui/src/app/app.module.ts
+++ controle-financeiro-ui/src/app/app.module.ts
@@ -7,7 +7,7 @@ import { AppComponent } from './app.component';
 
 import { TiposService } from './services/tipos.service';
 import { CategoriasService } from './services/categorias.service';
-import { ListagemCategoriasComponent } from './components/Categoria/listagem-categorias/listagem-categorias.component';
+import { ListagemCategoriasComponent, DialogExclusaoCategoriasComponent } from './components/Categoria/listagem-categorias/listagem-categorias.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatTableModule } from '@angular/material/table';
 import { MatIconModule } from '@angular/material/icon';
@@ -21,13 +21,15 @@ import { MatSelectModule } from '@angular/material/select';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { AtualizarCategoriaComponent } from './components/Categoria/atualizar-categoria/atualizar-categoria.component';
+import { MatDialogModule } from '@angular/material/dialog';
 
 @NgModule({
   declarations: [
     AppComponent,
     ListagemCategoriasComponent,
     NovaCategoriaComponent,
-    AtualizarCategoriaComponent
+    AtualizarCategoriaComponent,
+    DialogExclusaoCategoriasComponent
   ],
   imports: [
     BrowserModule,
@@ -43,7 +45,8 @@ import { AtualizarCategoriaComponent } from './components/Categoria/atualizar-ca
     MatInputModule,
     MatSelectModule,
     MatDividerModule,
-    MatGridListModule
+    MatGridListModule,
+    MatDialogModule
   ],
   providers: [
     TiposService,
--- controle-financeiro-ui/src/app/components/Categoria/listagem-categorias/listagem-categorias.component.ts
+++ controle-financeiro-ui/src/app/components/Categoria/listagem-categorias/listagem-categorias.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { MatTableDataSource} from '@angular/material/table';
+import { Component, Inject, OnInit } from '@angular/core';
+import { MatDialog, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatTableDataSource } from '@angular/material/table';
 import { CategoriasService } from 'src/app/services/categorias.service';
 
 @Component({
@@ -12,7 +13,9 @@ export class ListagemCategoriasComponent implements OnInit {
   categorias = new MatTableDataSource<any>();
   displayedColumns: string[];
 
-  constructor(private categoriaService: CategoriasService) { }
+  constructor(
+    private categoriaService: CategoriasService,
+    private dialog: MatDialog) { }
 
   ngOnInit(): void {
     this.categoriaService.PegarTodos().subscribe(resultado => {
@@ -22,8 +25,45 @@ export class ListagemCategoriasComponent implements OnInit {
     this.displayedColumns = this.ExibirColunas();
   }
 
-  ExibirColunas(): string[]{
+  ExibirColunas(): string[] {
     return ['nome', 'icone', 'tipo', 'acoes']
   }
 
+  AbrirDialog(categoriaId, nome): void {
+    this.dialog.open(DialogExclusaoCategoriasComponent, {
+      data: {
+        categoriaId: categoriaId,
+        nome: nome,
+      },
+    })
+      .afterClosed().subscribe((resultado) => {
+        if (resultado === true) {
+          this.categoriaService.PegarTodos().subscribe((dados) => {
+            this.categorias.data = dados;
+          });
+
+          this.displayedColumns = this.ExibirColunas();
+        }
+      });
+  }
+
+}
+
+@Component({
+  selector: 'app-dialog-exclusao-categorias',
+  templateUrl: 'dialog-exclusao-categorias.html',
+})
+export class DialogExclusaoCategoriasComponent {
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public dados: any,
+    private categoriasService: CategoriasService
+  ) { }
+
+  ExcluirCategoria(categoriaId): void {
+    this.categoriasService
+      .ExcluirCategoria(categoriaId)
+      .subscribe((resultado) => {
+
+      });
+  }
 }
