2b3845ef39416573b75a2d3a60853f458349da0e
Rodrigo <rodrigo.siqueira@rdsti.com.br>
2021-09-16 00:04:06 -0300

    Implementação repositótio genérico

---
+++ controle-financeiro-dal/Interfaces/IRepositorioGenerico.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace controle_financeiro_dal.Interfaces
+{
+    public interface IRepositorioGenerico<TEntity> where TEntity : class
+    {
+
+        IQueryable<TEntity> PegarTodos();
+
+        Task<TEntity> PegarPeloId(int id);
+
+        Task<TEntity> PegarPeloId(string id);
+
+        Task Inserir(TEntity entity);
+
+        Task Inserir(List<TEntity> entity);
+
+        Task Atualizar(TEntity entity);
+
+        Task Excluir(string id);
+        
+        Task Excluir(int id);
+
+    }
+
+}
